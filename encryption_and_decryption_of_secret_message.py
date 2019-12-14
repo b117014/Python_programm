f9a926a1da9b2bdc7556337e1e811c4860dc471d
b117014 <b117014@iiit-bh.ac.in>
2019-12-14 16:35:36 +0530

    encryption and decrypion

---
+++ encryption_and_decryption_of_secret_message.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 16:15:49 2019
+
+@author: root
+"""
+
+""" print("A=",ord("A"));  ord() method is used to retrieve ascii vale of alphabet
+print("65=" , chr(65));  chr() method retreive character of numeric value that privide to chr method as argument"""
+
+user_string = input("enter string with uppercase");
+secret_string = "";
+
+for char in user_string:
+    secret_string+=str(ord(char));
+print("secret message : " , secret_string);
+decoded_string="";
+for i in range(0, len(secret_string)-1 ,2):
+    char_code = secret_string[i]+secret_string[i+1];
+    decoded_string +=chr(int(char_code));
+    
+print(decoded_string)
