9507218bfe2dc4ea31702b1ac85cb6b7332d4891
b117014 <b117014@iiit-bh.ac.in>
2019-12-14 16:16:59 +0530

    logic behind print of string

---
+++ string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 15:56:25 2019
+
+@author: root
+"""
+
+tempString = "hey there, I am working on this project"
+
+print("length",len(tempString));   """ len() method returns length of string """
+
+
+print(tempString[0]);  """ print start character """
+
+print(tempString[-1]); """ print last character """
+
+print(tempString[0:4]); """ it will print 0th index to 3rd index """
+
+print(tempString[::-1]) ; """ print reverse string"""
