49bd075f48865717eee384e6028b49b1d4b2646f
b117014 <b117014@iiit-bh.ac.in>
2019-12-17 18:33:48 +0530

    word programm

---
+++ hk_rnk/hash.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 18:20:19 2019
+
+@author: root
+"""
+
+n= int (input())
+l = map(int,input().split())
+t = list(l)
+
+
+print(tuple(t))
