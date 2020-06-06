335c6518d47ff6277775cea526524c76ff02c162
b117014 <b117014@iiit-bh.ac.in>
2020-06-06 21:46:47 +0530

    start with bfs

--- data-science/machine-learning/opencv/videoCapture.py
+++ data-science/machine-learning/opencv/videoCapture.py
@@ -1,13 +1,16 @@
 import cv2
 import time
 
-video = cv2.VideoCapture(0)  # 0 for built in camera , 1 for external camera and 2 for third camera
+video = cv2.VideoCapture(-1)  # 0 for built in camera , 1 for external camera and 2 for third camera
 
 # video.read() method returns two value first is boolean which specify the opencv are able to open web cam
 # and second value is numpy array of frame of image
 check, frame = video.read()
-print(check)
-print(frame)
+
 
 time.sleep(2)
+cv2.imshow("Capturee", frame)
+cv2.waitKey(0)
+
+cv2.destroyAllWindow()
 video.release()
